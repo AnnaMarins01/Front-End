80500423a75a27039a9bc2c9ae40403eb958aea3
AnnaMarins01 <102561530+AnnaMarins01@users.noreply.github.com>
2023-06-26 01:19:21 -0300

    Add files via upload

---
+++ ProjetoPlaceHolder/js/build-table.js
@@ -0,0 +1,24 @@
+// https://www.valentinog.com/blog/html-table/ <- link que foi copiado o gerador
+// gera o cabeçalho da tabela
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+  
+//   gera a tabela
+  function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
